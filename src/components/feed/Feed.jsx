71e9379bd9cf9fbcbed90ff26a15a313d49af567
PaulSudipta2018 <sudiptapaul440@gmail.com>
2024-01-24 21:25:12 +0530

    @Sudipta-> committing the front end components and assets

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,23 @@
+import Share from '../Share/Share'
+import Post from '../post/Post'
+import './feed.css'
+
+export default function Feed() {
+  return (
+    <div className='feed'>
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  )
+}
